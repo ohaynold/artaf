e3030831f6e8a6a8a675d3da25a592aca69683a2
Oliver M. Haynold <oliverh@haynold.com>
2025-01-28 01:19:08 -0600

    Added runme.sh and process_data.py to give the user a convenient way to do the entire analysis in one go. For now, this just downloads some TAFs.

---
+++ artaf-python/process_data.py
@@ -0,0 +1,16 @@
+""" This is a script to run the data download and processing in logical order. It should
+be executed with the project directory as the working directory."""
+
+import meteostore
+
+
+def process_data():
+    """
+    Execute everything in the right order.
+    """
+    stations = meteostore.download_taf_stations()
+    meteostore.download_tafs(stations, 2024, 2024)
+
+
+if __name__ == "__main__":
+    process_data()
---
+++ runme.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+# This script runs the analysis in the logical order. It should be executed with the
+# project directory as the working directory.
+
+python3 artaf-python/process_data.py
