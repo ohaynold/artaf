c8bf66bbb138cf9f315c1b167d6798f47ba0d863
Oliver M. Haynold <oliverh@haynold.com>
2025-01-28 21:18:38 -0600

    The parser still needs some work, but we've got proof that we can do it. This thing now parses about half a million TAFs per minute on a single thread (though with pypy). No need to go multithreaded, it seems...

--- artaf-python/meteoparse/tafparser.py
+++ artaf-python/meteoparse/tafparser.py
@@ -1,49 +1,44 @@
-import csv
+import collections
 import datetime
-import io
-import zipfile
-import re
-import os.path
 import os
-from multiprocessing.managers import Token
-import meteostore
-
-import lark
+import os.path
 from enum import Enum
 
+import lark
+
+import meteostore
+
+
 class Amendment_Type(Enum):
-    NO_AMENDMENT = 0
     CORRECTED = 1
     AMENDED = 2
+
+
+ParsedForecast = collections.namedtuple(
+    "ParsedForecast",
+    ["aerodrome", "issued_at", "issued_in", "valid_from", "valid_until", "amendment",
+     "from_lines"])
+
+WeatherConditions = collections.namedtuple(
+    "WeatherConditions",
+    ["wind_speed"])
+
+FromLine = collections.namedtuple(
+    "FromLine",
+    ["valid_from", "valid_until", "conditions"]
+)
+
+
+def next_or_none(generator):
+    for a in generator:
+        return a
+    return None
 
 
 class TafTreeTransformer(lark.Transformer):
     def __init__(self, issue_date):
         super().__init__(self)
         self.issue_date = issue_date
-
-    def header(self, args):
-        amendment = list(filter(lambda x: x is Token and x.type == "header_amendment", args))
-        if len(amendment) == 0:
-            return Amendment_Type.NO_AMENDMENT
-        else:
-            return amendment[0]
-
-    def header_amendment(self, args):
-        s = args[0].value
-        if s == "COR":
-            return lark.Token(args[0].type, Amendment_Type.CORRECTED)
-        elif s == "AMD":
-            return lark.Token(args[0].type, Amendment_Type.AMENDED)
-        else:
-            raise ValueError("Impermissible amendment type '{}'.".format(s))
-
-    def day_hour_minute(self, args):
-        s = args[0].value
-        day = int(s[0:2])
-        hour = int(s[2:4])
-        minute = int(s[4:6])
-        return self.make_datetime_from_day_hour_minute(day, hour, minute)
 
     def make_datetime_from_day_hour_minute(self, day, hour, minute):
         if day >= self.issue_date.day:
@@ -53,8 +48,77 @@ class TafTreeTransformer(lark.Transformer):
         else:
             return datetime.datetime(self.issue_date.year + 1, 1, day, hour, minute)
 
-    def day_hour(self, args):
-        s = args[0].value
+    @staticmethod
+    def find_branches(args, target):
+        return [x for x in args if isinstance(x, lark.Tree) and x.data == target]
+
+    @staticmethod
+    def find_branch(args, target):
+        branches = [x for x in args if isinstance(x, lark.Tree) and x.data == target]
+        if len(branches) == 0:
+            return None
+        if len(branches) == 1:
+            return branches[0]
+        raise IndexError("Got more than one branch where I expected one.")
+
+    @staticmethod
+    def find_leaf(tree, target):
+        branches = [x for x in tree.children if isinstance(x, lark.Token) and x.type == target]
+        if len(branches) == 0:
+            return None
+        if len(branches) == 1:
+            return branches[0].value
+        raise IndexError("Got more than one leaf where I expected one.")
+
+    def start(self, args):
+        # TODO: ABE is shared between KABE and PABE. Maybe really have to key by center and aerodrome for PILs or re-sort after loading
+        issued_in = str(next(self.find_branch(args, "preamble").find_data("preamble_issued_in")).children[0])
+        aerodrome = str(next(self.find_branch(args, "header").find_data("header_issued_for")).children[0])
+        issued_at = str(next(self.find_branch(args, "header").find_data("header_issued_at")).children[0])
+        valid_from = str(next(self.find_branch(args, "header").find_data("header_valid_from")).children[0])
+        valid_until = str(next(self.find_branch(args, "header").find_data("header_valid_until")).children[0])
+        amendment = self.find_leaf(self.find_branch(args, "header"), "HEADER_AMENDMENT")
+
+        taf_content = self.find_branch(args, "taf_content")
+        if taf_content:
+            from_line_times = [valid_from]
+            from_lines = [taf_content.children[0]]
+            if len(taf_content.children) > 1:
+                for line in taf_content.children[1:]:
+                    from_line_times.append(line.children[0])
+                    from_lines.append(line.children[1])
+            from_line_times.append(valid_until)
+            new_from_line = [
+                FromLine(conditions=from_lines[i], valid_from=from_line_times[i], valid_until=from_line_times[i + 1])
+                for i in range(len(from_lines))]
+        else:
+            new_from_line = None
+
+        res = ParsedForecast(aerodrome=aerodrome, issued_at=issued_at, issued_in=issued_in,
+                             valid_from=valid_from, valid_until=valid_until, amendment=amendment,
+                             from_lines=new_from_line)
+
+        return res
+
+    def from_group_content(self, args):
+        conditions = self.find_branch(args, "from_conditions")
+        wind_group = next(conditions.find_data("wind_group"))
+        wind_speed = self.find_leaf(wind_group, "WIND_SPEED")
+        return WeatherConditions(wind_speed=wind_speed)
+        # TODO: Add other elements of group
+
+    def WIND_SPEED(self, arg):
+        return arg.update(value=int(arg.value))
+
+    def DAY_HOUR_MINUTE(self, args):
+        s = args.value
+        day = int(s[0:2])
+        hour = int(s[2:4])
+        minute = int(s[4:6])
+        return self.make_datetime_from_day_hour_minute(day, hour, minute)
+
+    def DAY_HOUR(self, args):
+        s = args.value
         day = int(s[0:2])
         hour = int(s[2:4])
         if hour == 24:
@@ -67,8 +131,17 @@ class TafTreeTransformer(lark.Transformer):
             res += datetime.timedelta(days=1)
         return res
 
+    def HEADER_AMENDMENT(self, tok):
+        if tok == "COR":
+            return tok.update(value=Amendment_Type.CORRECTED)
+        if tok == "AMD":
+            return tok.update(value=Amendment_Type.AMENDED)
+        return IndexError("Unknown amendment type.")
+
+
 _parser = None
-LARK_DIR = os.path.join("artaf-python","meteoparse","lark")
+LARK_DIR = os.path.join("artaf-python", "meteoparse", "lark")
+
 
 def parse_taf(message_time, message):
     global _parser
@@ -79,13 +152,18 @@ def parse_taf(message_time, message):
         tree = _parser.parse(message)
         transformer = TafTreeTransformer(message_time)
         tree = transformer.transform(tree)
-        # print(tree.pretty())
+        # print(tree)
         return True
     except lark.exceptions.UnexpectedInput as e:
         print("\n============================")
         print(message)
         print(e)
         print(e.get_context(message))
+        return False
+    except lark.exceptions.VisitError as e:
+        print("\n============================")
+        print(message)
+        print(e)
         return False
 
 
@@ -96,6 +174,8 @@ if __name__ == "__main__":
         for message_time, message in raw_tafs:
             if not parse_taf(message_time, message):
                 tafs_failed += 1
-            tafs_parsed +=1
+            tafs_parsed += 1
             if tafs_parsed % 1_000 == 0:
-                print("\r Parsed {} TAFs, failed {}, failure rate {}.     ".format(tafs_parsed, tafs_failed, tafs_failed/tafs_parsed), end="", flush=True)
+                print("\r Parsed {} TAFs, failed {}, failure rate {}.     ".format(tafs_parsed, tafs_failed,
+                                                                                   tafs_failed / tafs_parsed), end="",
+                      flush=True)
