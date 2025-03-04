34ca439a1160a59c73fcec8374d3560435a59c76
Oliver M. Haynold <oliverh@haynold.com>
2025-03-03 19:23:53 -0600

    Nice filenames for release and dates.

--- runme.sh
+++ runme.sh
@@ -14,7 +14,7 @@ python_command artaf-python/process_data.py "$@"
 # (artaf-python/process_data.py writes .current_data_set)
 mkdir -p output
 quarto render artaf-r/Report.qmd -P data_set:$(cat .current_data_set)
-mv .output/artaf-r/Report.pdf output
+mv .output/artaf-r/Report.pdf output/Haynold_Ylitalo_2025_Autoregressive.pdf
 
 echo "Finished!"
 echo "Please see the output in output/Report.pdf"
